326a774b7730961561f3ede8a13a64fd11cb1ee8
marirs <251870+marirs@users.noreply.github.com>
2023-10-03 12:52:24 +0530

    remove the hardcoded localhost from backend_api_env

--- sigmac_frontend/src/service.rs
+++ sigmac_frontend/src/service.rs
@@ -7,13 +7,22 @@ use gloo_net::http::Request;
 use std::env::var;
 use yew::Callback;
 
+const BACKEND_API_ENV: &str = "SIGMAC_API_HOST";
+
+fn get_backend_host() -> String {
+    match var(BACKEND_API_ENV) {
+        Ok(t) => t,
+        Err(_) => "".to_string(), // Same server so `/` is great
+    }
+}
+
 pub fn single_convert(
     sigma_rule: SigmaRuleData,
     callback: Callback<Result<SingleConvertResponse>>,
 ) {
-    let url = "/convert";
+    let url = format!("{}/convert", get_backend_host());
     wasm_bindgen_futures::spawn_local(async move {
-        let converted_rule = match Request::post(url).json(&sigma_rule) {
+        let converted_rule = match Request::post(url.as_str()).json(&sigma_rule) {
             Ok(req) => match req.send().await {
                 Ok(res) => match res.text().await {
                     Ok(t) => match serde_json::from_str::<SingleConvertResponse>(t.as_str()) {
@@ -34,10 +43,10 @@ pub fn batch_convert(
     sigma_rules: Vec<SigmaRuleData>,
     callback: Callback<Result<Vec<SingleConvertResponse>>>,
 ) {
-    let url = "/batch-convert";
+    let url = format!("{}/batch-convert", get_backend_host());
     wasm_bindgen_futures::spawn_local(async move {
         let batch_data = BatchSigmaRuleData { sigma_rules };
-        let converted_rule = match Request::post(url).json(&batch_data) {
+        let converted_rule = match Request::post(url.as_str()).json(&batch_data) {
             Ok(req) => match req.send().await {
                 Ok(res) => {
                     let res_text = res.text().await.unwrap();
@@ -59,9 +68,9 @@ pub fn batch_convert(
 }
 
 pub fn get_backends(callback: Callback<Result<Vec<String>>>) {
-    let url = "/backends";
+    let url = format!("{}/backends", get_backend_host());
     wasm_bindgen_futures::spawn_local(async move {
-        let backends = match Request::get(url).send().await {
+        let backends = match Request::get(url.as_str()).send().await {
             Ok(res) => {
                 let res_text = res.text().await.unwrap();
                 match serde_json::from_str::<Vec<String>>(res_text.as_str()) {
